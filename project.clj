0a1baf51478595eb6d4c463bda4b771adafb63c0
Jordan Lewis <jordan@knewton.com>
2013-03-16 16:25:25 -0400

    Add jdbc and korma etc

--- project.clj
+++ project.clj
@@ -9,7 +9,10 @@
                  [ring/ring-devel "1.1.0"]
                  [ring-basic-authentication "1.0.1"]
                  [environ "0.2.1"]
-                 [com.cemerick/drawbridge "0.0.6"]]
+                 [com.cemerick/drawbridge "0.0.6"]
+                 [org.clojure/java.jdbc "0.2.3"]
+                 [korma "0.3.0-beta7"]
+                 [postgresql "9.1-901.jdbc4"]]
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.2.1"]]
   :hooks [environ.leiningen.hooks]
